afc7cb93ca9b3c4fe0cb9d4a0f6c0df42e041991
孙俪 <1710741646@qq.com>
2019-01-05 08:04:32 +0800

    first  commit

--- sunli0103/src/main/java/com/sunli/sunli0103/network/ApiUtils.java
+++
@@ -1,9 +0,0 @@
-package com.sunli.sunli0103.network;
-
-/**
- * @Author sunli
- * @Data 2019/1/3
- */
-public class ApiUtils {
-    public static final String TYPE_GET_LIST_URL = "product/getProducts?pscid=%d";
-}
--- sunli0103/src/main/java/com/sunli/sunli0103/network/Constancts.java
+++
@@ -1,9 +0,0 @@
-package com.sunli.sunli0103.network;
-
-/**
- * @Author sunli
- * @Data 2019/1/3
- */
-public class Constancts {
-    public static final String TYPE_LIST_BODY_KEY = "pscid";
-}
